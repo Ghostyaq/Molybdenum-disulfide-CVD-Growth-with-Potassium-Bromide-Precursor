fbd67bbfbd419a63e92693026e990d0a2db61009
Ghostyaq <mitchellhung176@gmail.com>
2026-06-26 10:00:03 -0400

    added Gaussian Curve Analysis and PCA

--- LAS_testing.R
+++ LAS_testing.R
@@ -2,11 +2,7 @@ library(tidyverse)
 library(pracma)
 library(data.table)
 library(minpack.lm)
-
-file_path <- "data/default LAS/300x300/Large Area Scan.csv"
-
-raw <- read.csv(file_path, header = FALSE)
-data <- raw
+library(plotly)
 
 parse_LAS <- function(data){
     mat <- do.call(
@@ -17,7 +13,6 @@ parse_LAS <- function(data){
     return(as.data.frame(mat))   
 }
 
-data <- parse_LAS(data)
 tidy_conversion <- function(data) {
    x_axis <- data[, 1]
    data2 <- data |>
@@ -48,19 +43,19 @@ find_peak_locations <- function(data){
         
         results[[i-1]] <- tibble(
             id = i - 1,
-            x_axis_1 = x_axis[peak1],
-            intensity_1 = intensity[peak1],
-            x_axis_2 = x_axis[peak2],
-            intensity_2 = intensity[peak2]
+            x_axis1 = x_axis[peak1],
+            intensity1 = intensity[peak1],
+            x_axis2 = x_axis[peak2],
+            intensity2 = intensity[peak2]
         )
+        print(paste0("Spectrum ", (i - 1), " processed."))
     }
-    
     peak_locations <- bind_rows(results)
     peak_locations
 }
 
 gaussian <- function(x, A, mu, sigma) {
-    A * exp(-(x - mu)^2 / (2 * sigma^2))
+    A * exp(-(x - mu) ^ 2 / (2 * sigma ^ 2))
 }
 
 double_gaussian <- function(x, A1, mu1, sigma1, A2, mu2, sigma2, C) {
@@ -76,45 +71,33 @@ auto_gaussian_summary <- function(data, peak_locations) {
         spectrum_id <- peak_locations$id[i]
         y <- data[[spectrum_id + 1]]
         
-        A1_guess <- peak_locations$intensity_1[i]
-        mu1_guess <- peak_locations$x_axis_1[i]
-        
-        A2_guess <- peak_locations$intensity_2[i]
-        mu2_guess <- peak_locations$x_axis_2[i]
+        A1_guess <- peak_locations$intensity1[i]
+        A2_guess <- peak_locations$intensity2[i]
+        mu1_guess <- peak_locations$x_axis1[i]
+        mu2_guess <- peak_locations$x_axis2[i]
         
         fit <- tryCatch({
             nlsLM(y ~ double_gaussian(x, A1, mu1, sigma1, A2, mu2, sigma2, C),
-                start = list(
-                    A1 = A1_guess,
-                    mu1 = mu1_guess,
-                    sigma1 = 3,
-                    
-                    A2 = A2_guess,
-                    mu2 = mu2_guess,
-                    sigma2 = 3,
-                    
-                    C = min(y)
-                ),
-                
-                lower = c(0, 375, 0.5, 0, 395, 0.5, 0),
-                upper = c(Inf, 395, 15, Inf, 420, 15, Inf)
-            )
-        }, 
+                  start = list(
+                      A1 = A1_guess, mu1 = mu1_guess, sigma1 = 3,
+                      A2 = A2_guess, mu2 = mu2_guess, sigma2 = 3,
+                      C = min(y)
+                  ),
+                  lower = c(0, 375, 0.5, 0, 395, 0.5, 0),
+                  upper = c(Inf, 395, 15, Inf, 420, 15, Inf)
+                  )
+        },
         error = function(e) {
-            cat(
-                "Spectrum:",
-                spectrum_id,
-                "\n",
-                e$message,
-                "\n\n"
-            )
+            cat("Spectrum:", spectrum_id, "\n", e$message, "\n\n")
             NULL
         })
         
         if (is.null(fit)) {
             results[[i]] <- tibble(
-                id = spectrum_id, mu1 = 0, mu2 = 0, fwhm1 = 0, fwhm2 = 0
-                )
+                id = spectrum_id, mu1 = 0, mu2 = 0, fwhm1 = 0, fwhm2 = 0,
+                A1 = 0, A2 = 0, area1 = 0, area2 = 0, area_ratio = 0,
+                snr = 0, rmse = 0, r_squared = 0, diff = 0
+            )
             next
         }
         
@@ -122,28 +105,47 @@ auto_gaussian_summary <- function(data, peak_locations) {
         fwhm1 <- 2.35482 * p["sigma1"]
         fwhm2 <- 2.35482 * p["sigma2"]
         
+        area1 <- p["A1"] * p["sigma1"] * sqrt(2 * pi)
+        area2 <- p["A2"] * p["sigma2"] * sqrt(2 * pi)
+        area_ratio <- area1 / area2
+        
+        fitted_y <- double_gaussian(
+            x, p["A1"], p["mu1"], p["sigma1"], 
+            p["A2"], p["mu2"], p["sigma2"], p["C"]
+        )
+        residuals <- y - fitted_y
+        rmse <- sqrt(mean(residuals^2))
+        
+        ss_res <- sum(residuals^2)
+        ss_tot <- sum((y - mean(y))^2)
+        r_squared <- 1 - ss_res / ss_tot
+        
+        noise <- sd(residuals)
+        snr <- ifelse(noise == 0, Inf, max(y) / noise)
+        
         results[[i]] <- tibble(
-            id = spectrum_id,
-            mu1 = p["mu1"],
-            mu2 = p["mu2"],
-            fwhm1 = fwhm1,
-            fwhm2 = fwhm2
-            )
+            id = spectrum_id, mu1 = p["mu1"], mu2 = p["mu2"],
+            fwhm1 = fwhm1, fwhm2 = fwhm2, A1 = p["A1"], A2 = p["A2"],
+            area1 = area1, area2 = area2, area_ratio = area_ratio,
+            snr = snr, rmse = rmse, r_squared = r_squared,
+            diff = abs(p["mu2"] - p["mu1"])
+        )
     }
     bind_rows(results)
 }
 
-peak_summary <- find_peak_locations(data)
+# file_path <- "data/default LAS/300x300/Large Area Scan.csv"
+# raw <- read.csv(file_path, header = FALSE)
+# data <- raw
+# data <- parse_LAS(data)
 
-gaussian_results <- auto_gaussian_summary(
-    data,
-    peak_summary
-)
+peak_summary <- find_peak_locations(data)
+gaussian_results <- auto_gaussian_summary(data, peak_summary)
 
 peak_summary <- peak_summary |>
     mutate(
-        diff = abs(x_axis_1 - x_axis_2),
-        ratio = intensity_1 / intensity_2,
+        diff = abs(x_axis1 - x_axis2),
+        ratio = intensity1 / intensity2,
         ratio = ifelse(ratio > 1, ratio, 1/ratio)
     ) |>
     merge(gaussian_results, by = "id")
@@ -152,19 +154,51 @@ heatmap_df <- peak_summary |>
     mutate(
         x = ((id - 1) %% 300) + 1,
         y = ((id - 1) %/% 300) + 1,
-        diff = ifelse(intensity_1 > 730 & intensity_2 > 730, diff, 15),
-        diff = ifelse(diff > 26 | diff < 18, 15, diff),
-        ratio = ifelse(intensity_1 > 730 & intensity_2 > 730, ratio, 0.9),
-        ratio = ifelse(ratio > 1.25, 1.25, ratio),
-        mu1 = ifelse(intensity_1 > 730 & intensity_2 > 730, mu1, 0),
-        mu2 = ifelse(intensity_1 > 730 & intensity_2 > 730, mu2, 0),
-        fwhm1 = ifelse(intensity_1 > 730 & intensity_2 > 730, fwhm1, 0),
-        fwhm2 = ifelse(intensity_1 > 730 & intensity_2 > 730, fwhm2, 0)
-    ) 
+        curve = intensity1 > 730 & intensity2 > 730,
+        diff.x = ifelse(curve, diff.x, 15),
+        diff.x = ifelse(diff.x > 26 | diff.x < 18, 15, diff.x),
+        intensity_ratio = ifelse(curve, ratio, 0.9),
+        intensity_ratio = ifelse(intensity_ratio > 1.25, 1.25, intensity_ratio),
+        mu1 = ifelse(curve, mu1, 0),
+        mu2 = ifelse(curve, mu2, 0),
+        fwhm1 = ifelse(curve, fwhm1, 0),
+        fwhm2 = ifelse(curve, fwhm2, 0),
+        A1 = ifelse(curve, A1, 0),
+        A2 = ifelse(curve, A2, 0),
+        area1 = ifelse(curve, area1, 0),
+        area2 = ifelse(curve, area2, 0),
+        area_ratio = ifelse(curve, area_ratio, 0),
+        snr = ifelse(curve, snr, 0),
+        rmse = ifelse(curve, rmse, 0),
+        r_squared = ifelse(curve, r_squared, 0),
+        diff.y = ifelse(curve, diff.y, 0)
+    ) |>
+    select(id, curve, x, y, x_axis1, x_axis2, diff.x, mu1, mu2, diff.y, 
+           intensity1, intensity2, intensity_ratio, A1, A2, 
+           fwhm1, fwhm2, area1, area2, area_ratio, snr, rmse, r_squared)
+
+### PCA ANALYSIS ###
+pca_data <- data[data$V1 > 375 & data$V1 < 420, ]
+spectra_matrix <- t(as.matrix(pca_data[ , -1]))
+spectra_scaled <- scale(spectra_matrix)
+pca <- prcomp(spectra_scaled, center = TRUE, scale. = TRUE)
+
+#plot(cumsum(pca$sdev^2 / sum(pca$sdev^2)), type = "b", 
+#     xlab = "PC", ylab = "Cumulative Variance Explained")
+
+pca_scores <- as.data.frame(pca$x[, 1:5])
+pca_scores$id <- seq_len(nrow(pca_scores))
+heatmap_df <- cbind(heatmap_df, pca_scores[, 1:5])
+
+kmeans_vars <- heatmap_df |>
+    select(
+        diff.x, diff.y, mu1, mu2, intensity_ratio, A1, A2, fwhm1, fwhm2, 
+        area1, area2, area_ratio, snr, rmse, r_squared, PC1, PC2, PC3, PC4, PC5
+        )
 
 cluster_num <- 8
 clustering_results <- kmeans(
-    select(heatmap_df, diff, mu1, mu2, fwhm1, fwhm2, ratio), 
+    kmeans_vars, 
     center = cluster_num)
 ordering <- order(clustering_results$centers[, 1])
 mapping <- setNames(1:cluster_num, ordering)
@@ -177,7 +211,7 @@ p <- ggplot(heatmap_df, aes(x = x, y = y, fill = cluster)) +
     coord_equal() +
     scale_y_reverse() +
     scale_fill_gradientn(colors = c("red", "yellow", "blue")) + 
-    labs(fill = "Peak Separation") + 
+    labs(fill = "Clustering") + 
     theme_bw()
 p
 ggplotly(p)
